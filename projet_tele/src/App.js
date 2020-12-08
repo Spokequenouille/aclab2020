6a37dd2b30d7ab049cbb67b50a455d0e95e69135
unknown <lepinettenico@gmail.com>
2020-12-08 14:00:30 +0100

    first push

--- projet_tele/src/App.js
+++ projet_tele/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Coconut from './Coconut.js';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <Coconut/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ projet_tele/src/Coconut.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Coconut extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>Anastasia = la meuf de Seb</h1>
+        </div>);
+    }
+}
