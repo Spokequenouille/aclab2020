3bd9eecef09a94d661b806920be833040c39b0ee
Johann <johann.de-almeida@lacatholille.fr>
2020-12-08 11:19:02 +0100

    Suppression classe News | Modification des champs de la classe Classes$

--- src/main/java/com/example/aclab2020/Classes.java
+++ src/main/java/com/example/aclab2020/Classes.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Data
@@ -14,16 +13,18 @@ public class Classes {
     private int IdClasse;
     private String DateClasse;
     private String HeureClasse;
-    @OneToOne
-    @JoinColumn(name = "IdPromo")
-    private Promotions IdPromo_c;
-    @OneToOne
-    @JoinColumn(name = "IdSalle")
-    private Salles IdSalle_c;
-    @OneToOne
-    @JoinColumn(name = "IdMatiere")
-    private Matieres IdMatiere_c;
-    @OneToOne
-    @JoinColumn(name = "IdProf")
-    private Professeurs IdProf_c;
+    @ManyToOne
+    @JoinColumn(name = "promo_id", referencedColumnName = "IdPromo", nullable = false)
+    private Promotions promotions;
+    @ManyToOne
+    @JoinColumn(name = "salle_id", referencedColumnName = "IdSalle", nullable = false)
+    private Salles salles;
+    @ManyToOne
+    @JoinColumn(name = "matiere_id", referencedColumnName = "IdMatiere", nullable = false)
+    private Matieres matieres;
+    @ManyToOne
+    @JoinColumn(name = "prof_id", referencedColumnName = "IdProf", nullable = false)
+    private Professeurs professeurs;
+
+
 }
--- src/main/java/com/example/aclab2020/NewsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.aclab2020;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Integer> {
-}
