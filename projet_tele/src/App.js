3006e8864f3babcadf8afa5c548fd58149dfa16a
NicolasLepinette <55684429+NicolasLepinette@users.noreply.github.com>
2020-12-15 09:21:52 +0100

    Add files via upload

--- projet_tele/src/App.js
+++ projet_tele/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Logos/logo_droit.png';
 import './App.css';
 import Coconut from './Coconut.js';
 
@@ -9,17 +9,7 @@ function App() {
       <header className="App-header">
       <Coconut/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cliquez bande de bg
-        </a>
+
       </header>
     </div>
   );
