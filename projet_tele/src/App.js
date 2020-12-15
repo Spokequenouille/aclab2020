bb4a36587cb21b412fac27a9c4dd9903ef39a40f
guidezSeb <sebastien.guidez@lacatholille.fr>
2020-12-15 09:21:12 +0100

    clock OK

--- projet_tele/src/App.js
+++ projet_tele/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Coconut from './Coconut.js';
+import MyClock from './Clock.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
       <header className="App-header">
       <Coconut/>
+      <MyClock/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
